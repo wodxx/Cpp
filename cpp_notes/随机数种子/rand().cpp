b84da039897bbf09769444895042bd59cf106af9
wodxx <2286721671@qq.com>
2021-10-21 11:09:25 +0800

    add some notes

--- cpp_notes/随机数种子/rand().cpp
+++ cpp_notes/随机数种子/rand().cpp
@@ -1,8 +1,25 @@
 /*
  * @Author: your name
  * @Date: 2021-10-20 21:02:22
- * @LastEditTime: 2021-10-20 21:02:22
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-10-21 08:42:40
+ * @LastEditors: your name
  * @Description: In User Settings Edit
  * @FilePath: /Cpp/cpp_notes/随机数种子/rand().cpp
  */
+
+#include <iostream>
+#include <stdlib.h>
+#incldue <time.h>
+
+int main(){
+  srand((unsigned)time(0));
+  cout<<" Ten random numbers from"<<MIN<<
+  "to"<<MAX<<endl;
+
+  for(int i = 0; i < 10; i++){
+    cout<<MIN + (int)MAX * rand() / (RAND_MAX + 1)<<"/t";
+  }
+
+  cout<<endl;
+  return 0;
+}
--- cpp_notes/随机数种子/srand().cpp
+++ cpp_notes/随机数种子/srand().cpp
@@ -1,8 +1,27 @@
 /*
  * @Author: your name
  * @Date: 2021-10-20 21:15:49
- * @LastEditTime: 2021-10-20 21:15:49
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
+ * @LastEditTime: 2021-10-20 22:07:20
  * @FilePath: /Cpp/cpp_notes/随机数种子/srand().cpp
  */
+
+ 
+#include <iostream> 
+using namespace std; 
+#include <stdlib.h> 
+#include <time.h> 
+#define MIN 0 //随机数产生的范围 
+#define MAX 99 
+
+
+int main() 
+{ 
+    srand((unsigned)time(NULL)); 
+    cout<<"Ten random numbers from "<<MIN<< " to "<<MAX<<" :/n"<<endl; 
+    for(int i=0; i<10; i++) //产生随机数 
+    { 
+        cout<<MIN + rand() % (MAX + MIN - 1)<<"/t"; 
+    } 
+    cout<<endl; 
+    return 0; 
+} 
