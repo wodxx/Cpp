26a04dc3a56ca4c98d8c63d2f06621fff3bb96fd
wodxx <2286721671@qq.com>
2021-10-24 10:20:17 +0800

    some notes

--- 数据结构/字符串/5.replaceblack_1.cpp
+++ 数据结构/字符串/5.replaceblack_1.cpp
@@ -1,8 +1,29 @@
 /*
  * @Author: your name
  * @Date: 2021-10-21 11:04:10
- * @LastEditTime: 2021-10-21 11:04:10
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
+ * @LastEditTime: 2021-10-21 11:10:36
  * @FilePath: /Cpp/数据结构/字符串/5.replaceblack_1.cpp
+   剑指offer NO.5:替换法！
  */
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string replaceSpace(string s) {
+        string s2;
+        for(size_t i = 0; i < s.size(); i++){
+            if (s[i] == ' ') {
+                s2 += "%20";
+            } else {            //这里为了循环清楚，要写else
+                s2 += s[i];
+            }
+        }
+        return s2;
+    }
+};
+
+int main(){
+  return 0;
+}
--- 数据结构/字符串/5.replaceblack_2.cpp
+++ 数据结构/字符串/5.replaceblack_2.cpp
@@ -1,8 +1,41 @@
 /*
  * @Author: your name
  * @Date: 2021-10-21 08:45:03
- * @LastEditTime: 2021-10-21 08:45:03
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
+ * @LastEditTime: 2021-10-21 11:10:37
  * @FilePath: /Cpp/数据结构/字符串/replaceblack.cpp
+   剑指offer NO.5:双指针法！
  */
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string replaceSpace(string s) {
+      int space = 0;//空格数量
+      int len = s.size();
+      for(int i = 0; i < len; i++){
+        if (s[i] == ' ') space++;
+      }
+      s.resize(len + 2 * space);
+      //resize()重新定义长度函数
+      int i = len;
+      int j = s.size();
+      //这里已经重新计算了长度，故这里的s.size（）已经是新长度
+      while(i >= 0 && j > i){  //循环截至的条件是两指针相等，i可以等于0
+        if(s[i] == ' '){
+          s[j--] = '0';  //字符串元素用单引号
+          s[j--] = '2';
+          s[j--] = '%';
+        } else {
+          s[j--] = s[i];
+        }
+        i--;
+      }
+    return s;
+    }
+};
+
+int main(){
+  return 0;
+}
